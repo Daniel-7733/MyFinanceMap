b598f0aad822260f14c1f1e67c1827405848fffa
Daniel Kazemian <achilles.7733@gmail.com>
2026-02-03 20:42:05 +0300

    Add size to graph

--- app/static/js/dashboard_charts.js
+++ app/static/js/dashboard_charts.js
@@ -1,3 +1,9 @@
+// This function give width & height size for a chart
+const handleResize = (chart) => {
+chart.resize(500, 500);
+}
+
+// Bar chart; income vs expanse
 (() => {
   "use strict";
 
@@ -10,7 +16,7 @@
     const ctx = canvas.getContext("2d");
 
     new Chart(ctx, {
-      type: "line", // try "line" too 1) "bar" 2) "line"
+      type: "bar", // try "line" too 1) "bar" 2) "line"
       data: {
         labels: chartData.labels,
         datasets: [
@@ -26,6 +32,7 @@
       },
       options: {
         responsive: true,
+        onResize: handleResize,
         scales: {
           y: {
             beginAtZero: true,
@@ -35,3 +42,7 @@
     });
   });
 })();
+
+
+// line chart; total expanses in a specific month
+
