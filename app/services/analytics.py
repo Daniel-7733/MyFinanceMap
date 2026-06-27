8229f522a39b7c398edf42038259e3a1d19ed86f
Daniel Kazemian <achilles.7733@gmail.com>
2026-06-27 23:37:58 +0300

    statistics

--- app/services/analytics.py
+++ app/services/analytics.py
@@ -452,3 +452,29 @@ def monthly_income_by_category(user_id: int, last_n: int = 6) -> list[dict[str, 
 
     return data
 
+
+def statistics(amounts: list[Decimal]) -> dict[str, Decimal]:
+    """
+    Function to calculate the expense statistics. Note: if the amount is empty, the function return 0 for all
+    maximum, minimum, and average.
+    :param amounts: (Decimal) a list of amount.
+    :return: maximum, minimum, and average amount.
+    """
+    zero = Decimal("0")
+
+    if not amounts:
+        return {
+            "min": zero,
+            "max": zero,
+            "average": zero,
+        }
+
+    total_expense: Decimal = sum(amounts)
+    count: Decimal = Decimal(len(amounts))
+
+    return {
+        "min": min(amounts),
+        "max": max(amounts),
+        "average": total_expense / count,
+    }
+
